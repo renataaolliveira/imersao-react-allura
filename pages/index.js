aff49162a19ab1f3855613bfae11c8b2fd757de7
Renata Oliveira <renatadeoliveiramelo@gmail.com>
2021-01-30 12:20:30 -0300

    codigo completo da primeira aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,55 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import QuizzBackground from '../src/components/QuizzBackground'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
+
+const QuizzContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizzBackground backgroundImage={db.bg}>
+      <QuizzContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>DIVAS POP</h1>
+          </Widget.Header>
+          <Widget.Content>
+          
+
+
+
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Quizzes da Galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+          
+
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizzContainer>
+    </QuizzBackground>
+
+  ) 
 }
---
+++ src/components/Widget/index.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+//Linkar os componentes com . como se fossem objetos permite que componentes herdem estilos e propriedades de outros
+//Logo, se exportarmos o componente Widget, todos os outros que estão associados a ele serão exportados também
+//Caso não estivessem linkados, deveriam ser exportados um por um
+
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid ${({ theme }) => theme.colors.secondary};
+  background-color: #1C1814;
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-weight: 1;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`;
+
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  * {
+    margin: 0;
+  }
+`;
+
+Widget.Content = styled.header`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+export default Widget;

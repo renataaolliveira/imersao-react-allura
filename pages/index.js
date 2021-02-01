0c6a2469d5bbe5771aed352803f3a2978b98c229
Renata Oliveira <renatadeoliveiramelo@gmail.com>
2021-02-01 19:19:26 -0300

    Final da aula 02

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import db from '../db.json'
 import Widget from '../src/components/Widget'
 import Footer from '../src/components/Footer'
 import QuizzBackground from '../src/components/QuizzBackground'
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 const Title = styled.h1`
   font-size: 50px;
@@ -24,18 +26,35 @@ const QuizzContainer = styled.div`
 
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizzBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quizz Alura - Divas Pop</title>
+      </Head>
       <QuizzContainer>
         <Widget>
           <Widget.Header>
             <h1>DIVAS POP</h1>
           </Widget.Header>
           <Widget.Content>
-          
-
-
-
+            <form onSubmit={function(event) {
+              //Evita o comportamento padrão de submeter o formulário para o servidor
+                event.preventDefault();
+                router.push(`./quizz?name=${name}`);
+            }}>
+              <input 
+                onChange={function(event) {
+                  setName(event.target.value);
+                }}
+                placeholder="Digite seu nome" 
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Bora, {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>

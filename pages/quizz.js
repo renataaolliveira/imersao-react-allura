fa058a4e2421b4c0ac9bdde720ba5841d51aaf1e
Renata Oliveira <renatadeoliveiramelo@gmail.com>
2021-02-06 18:26:53 -0300

    fim da aula 3

--- pages/quizz.js
+++ pages/quizz.js
@@ -5,7 +5,27 @@ import QuizzBackground from '../src/components/QuizzBackground';
 import QuizzContainer from '../src/components/QuizzContainer';
 import Button from '../src/components/Button';
 
-function QuestionWidget({ question, totalQuestions, questionIndex }) {
+function LoadingWidget() {
+    return (
+      <Widget>
+        <Widget.Header>
+          Carregando...
+        </Widget.Header>
+  
+        <Widget.Content style={{ display: 'flex', justifyContent: 'center' }}>
+          {/*<Lottie
+            width="200px"
+            height="200px"
+            className="lottie-container basic"
+            config={{ animationData: loadingAnimation, loop: true, autoplay: true }}
+          />*/}
+        </Widget.Content>
+      </Widget>
+    );
+  }
+
+function QuestionWidget({ question, totalQuestions, questionIndex, onSubmit }) {
+    const questionId = `question__${questionIndex}`;
     return (
         <Widget>
             <Widget.Header>
@@ -31,10 +51,23 @@ function QuestionWidget({ question, totalQuestions, questionIndex }) {
                     {question.description}
                 </p>
 
-                {question.alternatives.map((alternative) => {
-                    return alternative;
-                })}
-                <Button>
+                <form onSubmit={(infosDoEvento) => {
+                    infosDoEvento.preventDefault();
+                    onSubmit();
+                }}>
+                    {question.alternatives.map((alternative, alternativeIndex) => {
+                        const alternativeId = `alternative__${alternativeIndex}`;
+                        return (
+                            <Widget.Topic as="label" htmlFor={alternativeId}>
+                                <input style={ {display:'none'} } id={alternativeId} name={questionId} type="radio" />
+                                {alternative}
+                            </Widget.Topic>
+                        );   
+                    })}
+                </form>
+                
+
+                <Button type="submit">
                     Pronto!
                 </Button>
             </Widget.Content>
@@ -43,19 +76,48 @@ function QuestionWidget({ question, totalQuestions, questionIndex }) {
 }
 
 
+const screenStates = {
+    QUIZZ: 'QUIZZ',
+    LOADING: 'LOADING',
+    RESULT: 'RESULT',
+}
+
 export default function QuizzPage() {
+    const [screenState, setScreenState] = React.useState(screenStates.LOADING);
     const totalQuestions = db.questions.length;
-    const questionIndex = 0;
+    const [questionIndex, setQuestionIndex] = React.useState(0);
     const question = db.questions[questionIndex];
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setScreenState(screenStates.QUIZZ);
+        }, 1000);
+    }, []);
+    
+    function handleSubmit() {
+        const nextQuestion = questionIndex + 1;
+        if (nextQuestion < totalQuestions) {
+            setQuestionIndex(nextQuestion);
+        } else {
+            setScreenState(screenStates.RESULT);
+        };   
+    };
 
     return (
         <QuizzBackground backgroundImage={db.bg}>
             <QuizzContainer>
-                <QuestionWidget 
-                question={question}
-                questionIndex={questionIndex} 
-                totalQuestions={totalQuestions}
-                />
+
+                {screenState === screenStates.QUIZZ && (
+                    <QuestionWidget 
+                    question={question}
+                    questionIndex={questionIndex} 
+                    totalQuestions={totalQuestions}
+                    onSubmit={handleSubmit}
+                    />)}
+
+                {screenState === screenStates.LOADING && <LoadingWidget />}
+
+                {screenState === screenStates.RESULT && <div>Você acertou X questões, parabéns!</div>}
             </QuizzContainer>
         </ QuizzBackground>
     );
